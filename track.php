f59cde55b2ef956a6c3909240888d070dcfb1e6b
Simon Tang <stang@softwaresecure.com>
2014-07-11 22:22:40 -0400

    a

---
+++ track.php
@@ -0,0 +1,18 @@
+<?php
+
+//$_POST["name"]
+//http://www.newegg.com/Product/Product.aspx?Item=N82E16819116989
+
+parse_str("http://www.newegg.com/Product/Product.aspx?Item=N82E16819116989",$output);
+var_dump($output);
+
+$doc = array(
+	"_id" => 
+    "name" => "MongoDB",
+    "type" => "database",
+    "count" => 1,
+    "info" => (object)array( "x" => 203, "y" => 102),
+    "versions" => array("0.9.7", "0.9.8", "0.9.9")
+);
+
+?>
