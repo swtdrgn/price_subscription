0951f0b2d65946acd9cd969572e20482ffd48901
Simon Tang <stang@softwaresecure.com>
2014-07-11 22:50:13 -0400

    l

--- track.php
+++ track.php
@@ -1,7 +1,15 @@
 <html>
-<?php
+Name: <?=$_POST["name"]?>
+
+<?
+	//$_POST["name"]
+	//http://www.newegg.com/Product/Product.aspx?Item=N82E16819116989
+
 	phpinfo();
+
+	parse_str($_POST["name"],$output);
+	var_dump($output);
 ?>
 
-Name: <?=$_POST["name"]?>
+<br/><br/>
 </html>
