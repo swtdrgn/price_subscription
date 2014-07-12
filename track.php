95ab622ab45e63709eeb2ad39feb4476da65bf31
Simon Tang <stang@softwaresecure.com>
2014-07-11 23:02:24 -0400

    p

--- track.php
+++ track.php
@@ -3,8 +3,8 @@ Name: <?=$_POST["name"]?><br/>
 
 <?
 	$parsed_url = parse_url($_POST["name"]);
-	var_dump($parsed_url);
-	parse_str($parsed_url["param"],$output);
+	//var_dump($parsed_url);
+	parse_str($parsed_url["query"],$output);
 	var_dump($output);
 ?>
 
