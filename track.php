e4b2bc2c258e2270fec4d1eb27dfa40ae709a838
Simon Tang <stang@softwaresecure.com>
2014-07-12 07:47:29 -0400

    a

--- track.php
+++ track.php
@@ -3,10 +3,17 @@ Name: <?=$_POST["name"]?><br/>
 
 <?
 	$parsed_url = parse_url($_POST["name"]);
-	//var_dump($parsed_url);
 	parse_str($parsed_url["query"],$output);
 	var_dump($output);
-?>
 
+	$subscribe_email = array('$addToSet' => array("email" => $_POST["email"]));
+
+	$doc = array("_id" => $output["Item"]);
+
+	$connection = new MongoClient();
+	$db = $connection->neweggtracker;
+	$db->update($doc,$subscribe_email);
+?>
+singleFinalPrice
 <br/><br/>
 </html>
