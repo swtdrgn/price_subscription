6596e7680f44d4b2956cab6a6fcab923adb46dc9
Simon Tang <stang@softwaresecure.com>
2014-07-12 08:41:05 -0400

    fjklsd

--- track.php
+++ track.php
@@ -9,10 +9,13 @@ Name: <?=$_POST["name"]?><br/>
 
 	$doc = array("_id" => $output["Item"]);
 
+	echo "<br/>Here 01?<br/>";
 	$connection = new MongoClient();
 	var_dump($connection);
+	echo "<br/>Here 02?<br/>";
 	$db = $connection->neweggtracker;
 	var_dump($db);
+	echo "<br/>Here 03?<br/>";
 	$collection = $db->subscription;
 	var_dump($collection);
 	var_dump($collection->update($doc,$subscribe_email));
