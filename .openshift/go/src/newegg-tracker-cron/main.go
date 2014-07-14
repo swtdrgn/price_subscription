4bd54d5b348a12e9f5e5caf2d65b2297213d2189
Simon Tang <stang@softwaresecure.com>
2014-07-14 15:26:13 -0400

    g

--- .openshift/go/src/newegg-tracker-cron/main.go
+++ .openshift/go/src/newegg-tracker-cron/main.go
@@ -13,9 +13,16 @@ type msg struct {
   Msg   string        `bson:"msg"`
   Count int           `bson:"count"`
 }
+
+type subscriptions struct {
+  Id              bson.ObjectId          `bson:"_id"`
+  Subscriptions   map[string]string      `bson:"subscriptions"`
+  Prices          map[string]float64     `bson:"prices"`
+}
  
 func main() {
 	uri := os.Getenv("OPENSHIFT_MONGODB_DB_URL")
+	fmt.Println(uri)
 	if uri == "" {
 		fmt.Println("no connection string provided")
 		os.Exit(1)
@@ -27,19 +34,40 @@ func main() {
 	}
 	defer sess.Close()
 
-	sess.SetSafe(&mgo.Safe{})
-	db := sess.DB("subscriptions")
-	//collection := sess.DB("subscriptions").C("foo")
-	collectionNames,err := db.CollectionNames()
+	fmt.Println(sess.DB("neweggtracker").CollectionNames())
 
-	for _,collectionName := range collectionNames {
-		collection := db.C(collectionName);
-		err = collection.Update(bson.M{"_id": collectionName}, bson.M{"$set": bson.M{"prices",bson.M{time.Now(),12.2}}})
+	sess.SetSafe(&mgo.Safe{})
+	collection := sess.DB("neweggtracker").C("subscriptions")
+	fmt.Println(collection)
+	fmt.Println("Here...")
+	//collection := sess.DB("subscriptions").C("foo")
+	//collectionNames,err := db.CollectionNames()
+	//it := db.Find(nil).Iter() //.All(&results)
+
+	var results interface{}
+	fmt.Println("Finding...")
+	fmt.Println(collection.Find(nil))
+	fmt.Println("Found...")
+	fmt.Println(results)
+
+	var subscription subscriptions;
+	for it := collection.Find(nil).Iter(); it.Next(subscription); {
+		fmt.Println("Updating...")
+		err = collection.Update(bson.M{"_id": subscription.Id}, bson.M{"$set": bson.M{"prices": bson.M{time.Now().String(): 12.2}}})
 		if err != nil {
 			fmt.Printf("Can't update document %v\n", err)
 			os.Exit(1)
 		}
 	}
+
+	/*for _,collectionName := range collectionNames {
+		collection := db.C(collectionName);
+		err = collection.Update(bson.M{"_id": collectionName}, bson.M{"$set": bson.M{"prices": bson.M{time.Now().String(): 12.2}}})
+		if err != nil {
+			fmt.Printf("Can't update document %v\n", err)
+			os.Exit(1)
+		}
+	}*/
 
 	/*doc := msg{Id: bson.NewObjectId(), Msg: "Hello from go"}
 	err = collection.Insert(doc)
