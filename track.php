eab9e71233f44f4f1b21e66399d6a7a54d370d9e
Simon Tang <stang@softwaresecure.com>
2014-07-17 15:07:42 -0400

    handle null items

--- track.php
+++ track.php
@@ -5,14 +5,18 @@ Name: <?=$_POST["name"]?><br/>
 	$parsed_url = parse_url($_POST["name"]);
 	parse_str($parsed_url["query"],$output);
 
-	$doc = array("_id" => $output["Item"]);
-	$subscribe_email = array('$addToSet' => array("email" => $_POST["email"]));
-	$update_options = array('upsert' => true);
+	if (!empty($output["Item"])) {
+		$doc = array("_id" => $output["Item"]);
+		$subscribe_email = array('$addToSet' => array("email" => $_POST["email"]));
+		$update_options = array('upsert' => true);
 
-	$connection = new MongoClient($_ENV["OPENSHIFT_MONGODB_DB_URL"]);
-	$db = $connection->neweggtracker;
-	$subscription_collection = $db->subscriptions;
-	$subscription_collection->update($doc,$subscribe_email,$update_options);
+		$connection = new MongoClient($_ENV["OPENSHIFT_MONGODB_DB_URL"]);
+		$db = $connection->neweggtracker;
+		$subscription_collection = $db->subscriptions;
+		$subscription_collection->update($doc,$subscribe_email,$update_options);
+	} else {
+		echo "empty."
+	}
 
 	//$subscribe_email = array('$set' => array('prices' => array('date' => )));
 	//$subscribe_email = array('prices' => array('$set' => array('date' => date("y.m.d"), 'price' => 35.23)));
